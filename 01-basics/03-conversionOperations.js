e9ff54415700073ac9022737c949655ce508c470
Hamzaamir96 <m.hamzaamir96@gmail.com>
2024-09-18 11:15:52 +0000

    conversion of datatypes

---
+++ 01-basics/03-conversionOperations.js
@@ -0,0 +1,14 @@
+let score = "Ayyan"
+//console.log(typeof score);
+//console.log(typeof (score));
+let valueInNumber=Number(score)
+//console.log( valueInNumber);
+let isLoggedIn ="true"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"Ayyan"=>true
+let someNumber = 33
+let stringNumber =String(someNumber)
+console.log( typeof stringNumber);

026356fc4a301ebfbb195825d9d8b18f150c96f1
Hamzaamir96 <m.hamzaamir96@gmail.com>
2024-09-18 09:10:21 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="Ayyan@google.com"
+var accountPassword="12345"
+accountCity="Lahore"
+// accountId=2 //not allowed
+accountEmail="ayyan@google.com"
+accountPassword="2345"
+accountCity="Karachi"
+let accountState;
+console.log(accountId );
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all JS code as newer version 
+//alert(3+3)//we are using nodeJS not browser 
+let name ="Ayyan"
+let age=11
+let isLoggedIn=false
+//number=>2to power of53
+//bignit
+//string=>""
+//boolean=>true,false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object 
+console.log(typeof"Ayyan")
